57ffa522671b672f53cfce4a3030b33a66bc5573
nitinmandloi26 <117780257+nitinmandloi26@users.noreply.github.com>
2025-09-14 04:46:38 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:"export",
-    basePath: '/FormZ',
-    assetPrefix: '/FormZ/'
+  output: "export",
+  basePath: "/FormZ",      // repo name
+  assetPrefix: "/FormZ/",  // makes static assets load
+  images: {
+    unoptimized: true,     // <-- important for GitHub Pages
+  },
 };
 
 export default nextConfig;
